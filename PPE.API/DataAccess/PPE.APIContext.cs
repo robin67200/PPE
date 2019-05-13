b55f1502e4d157ccaa723bee9df2059ee263946a
jejeOtt <joottjeremy3@gmail.com>
2019-05-14 00:00:17 +0200

    student edit button

--- PPE.API/DataAccess/PPE.APIContext.cs
+++ PPE.API/DataAccess/PPE.APIContext.cs
@@ -21,7 +21,8 @@ namespace PPE.API.Models
 
         public DbSet<PPE.API.Models.Critere> Criteres { get; set; }
         public DbSet<PPE.API.Models.Note> Notes { get; set; }
-        public DbSet<PPE.API.Models.User> Users {get; set;}
+        public object User { get; internal set; }
+       public DbSet<PPE.API.Models.User> Users {get; set;}
     
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
