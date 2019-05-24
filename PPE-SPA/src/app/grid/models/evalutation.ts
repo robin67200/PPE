51601dbe7c9674de2026b1dce07bb572c115178f
jejeOtt <joottjeremy3@gmail.com>
2019-05-24 16:38:57 +0200

    evaluation

---
+++ PPE-SPA/src/app/grid/models/evalutation.ts
@@ -0,0 +1,12 @@
+export class Evaluation {
+    id: number;
+
+    constructor(
+      public date: Date,
+      public EtudiantId: number,
+      public JuryId: number,
+    ) {
+      this.id = 0;
+    }
+
+  }
