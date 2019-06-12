49c776c459c7631c4921c08c149d37cffc57a4b0
robin67200 <48128624+robin67200@users.noreply.github.com>
2019-06-12 18:24:53 +0200

    calcul final

--- PPE-SPA/src/app/grid/grid-create/grid-create.component.ts
+++ PPE-SPA/src/app/grid/grid-create/grid-create.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Evaluation } from '../models/evalutation';
 import { Note } from '../models/Note';
-import { Critere } from '../models/Critere';
-import { Phase } from '../models/Phase';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { GridService } from '../service/grid.service';
 import { Student } from 'src/app/student/models/student';
@@ -21,6 +19,9 @@ export class GridCreateComponent implements OnInit {
   note = new Note(0, 0, 0);
   student: Student = new Student('', '', '', '', '', '');
   sum: number;
+  sumB: number;
+  sumPenalite: number;
+  sumNoteFinale: number;
 
   constructor(
     route: ActivatedRoute,
@@ -37,5 +38,18 @@ export class GridCreateComponent implements OnInit {
   calculatePhase1(premier: number, second: number, troisieme: number, quatrieme: number) {
       this.sum = +premier + +second + +troisieme + +quatrieme;
   }
+
+  calculatePhase2(premierB: number, secondB: number, troisiemeB: number, quatriemeB: number, cinquiemeB: number) {
+    this.sumB = +premierB + +secondB + +troisiemeB + +quatriemeB + +cinquiemeB;
+  }
+
+  calculatePenalite(penaliteA: number, penaliteB: number) {
+    this.sumPenalite = +penaliteA + +penaliteB;
+  }
+
+  calculateNoteFinale() {
+    this.sumNoteFinale = +this.sum + +this.sumB - +this.sumPenalite ;
+  }
+
 
 }
